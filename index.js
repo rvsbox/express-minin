b747e4c71ce7c2559df9990ba1007f6b1648eb73
rvsbox <rvsbox@gmail.com>
2020-02-24 01:33:18 +0200

    4-07 Добавление пользователя

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
 
 const app = express()
 
@@ -18,6 +19,18 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5e52f8c54c61e90edb48f188')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
@@ -36,6 +49,15 @@ async function start() {
     // const url = `mongodb://root:mogaba@127.0.0.1:27017/test?authSource=admin`
     // await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     await mongoose.connect(configDB.dbURI, configDB.dbOptions)
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'vladilen@mail.ru',
+        name: 'Vladilen',
+        cart: {items: []}
+      })
+      await user.save()
+    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
