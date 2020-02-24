90e83564e1901ebcd9b1997ef0fb3105814a4b32
rvsbox <rvsbox@live.ru>
2020-02-24 23:28:49 +0200

    5-03 Сохранение сессии

--- index.js
+++ index.js
@@ -25,15 +25,15 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('5e52f8c54c61e90edb48f188')
-    req.user = user
-    next()
-  } catch (e) {
-    console.log(e)
-  }
-})
+// app.use(async (req, res, next) => {
+//   try {
+//     const user = await User.findById('5e52f8c54c61e90edb48f188')
+//     req.user = user
+//     next()
+//   } catch (e) {
+//     console.log(e)
+//   }
+// })
 
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -63,12 +63,12 @@ async function start() {
     await mongoose.connect(configDB.dbURI, configDB.dbOptions)
     const candidate = await User.findOne()
     if (!candidate) {
-      const user = new User({
-        email: 'vladilen@mail.ru',
-        name: 'Vladilen',
-        cart: {items: []}
-      })
-      await user.save()
+    //   const user = new User({
+    //     email: 'vladilen@mail.ru',
+    //     name: 'Vladilen',
+    //     cart: {items: []}
+    //   })
+    //   await user.save()
     }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
