bf5fab2bd27cdca61d1c628b7c2a1dd8b5794804
rvsbox <rvsbox@live.ru>
2020-02-24 20:40:21 +0200

    4-12 Подготовка страницы заказов

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
 const User = require('./models/user')
 
@@ -39,6 +40,7 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
 
 
 
