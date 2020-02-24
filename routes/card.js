1221646a3e21943292c358a44c147c8a47571f21
rvsbox <rvsbox@live.ru>
2020-02-24 20:16:35 +0200

    4-09 Отображение корзины

--- routes/card.js
+++ routes/card.js
@@ -2,6 +2,19 @@ const {Router} = require('express')
 const Course = require('../models/course')
 const router = Router()
 
+
+function mapCartItems(cart) {
+  return cart.items.map(c => ({
+    ...c.courseId._doc, count: c.count
+  }))
+}
+
+function computePrice(courses) {
+  return courses.reduce((total, course) => {
+    return total += course.price * course.count
+  }, 0)
+}
+
 router.post('/add', async (req, res) => {
   const course = await Course.findById(req.body.id)
   await req.user.addToCart(course)
@@ -14,14 +27,18 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-  // const card = await Card.fetch()
-  // res.render('card', {
-  //   title: 'Корзина',
-  //   isCard: true,
-  //   courses: card.courses,
-  //   price: card.price
-  // })
-  res.json({test: true})
+  const user = await req.user
+    .populate('cart.items.courseId')
+    .execPopulate()
+
+  const courses = mapCartItems(user.cart)
+
+  res.render('card', {
+    title: 'Корзина',
+    isCard: true,
+    courses: courses,
+    price: computePrice(courses)
+  })
 })
 
 module.exports = router
